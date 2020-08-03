d65e2dca14d9e1ccfd75037a30f9db7af8ca6c7a
guiguat <guaturadzn@gmail.com>
2020-08-03 18:13:36 -0300

    create teacher form component

--- web/src/Pages/TeacherForm/index.tsx
+++ web/src/Pages/TeacherForm/index.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import PageHeader from "../../Components/PageHeader";
 
 // import { Container } from './styles';
 
 const TeacherForm: React.FC = () => {
-  return <h1>TeacherForm</h1>;
+  return (
+    <div id="page-teacher-form" className="container">
+      <PageHeader title="Que incrível que você quer dar aulas." />
+    </div>
+  );
 };
 
 export default TeacherForm;
