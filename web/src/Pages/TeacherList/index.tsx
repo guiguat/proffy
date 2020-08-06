08adcdb25b81806c3469ee6119be575941111f58
guiguat <guaturadzn@gmail.com>
2020-08-06 18:44:04 -0300

    update inputs

--- web/src/Pages/TeacherList/index.tsx
+++ web/src/Pages/TeacherList/index.tsx
@@ -4,14 +4,41 @@ import "./styles.css";
 import PageHeader from "../../Components/PageHeader";
 import TeacherItem from "../../Components/TeacherItem";
 import Input from "../../Components/Input";
+import Select from "../../Components/Select";
 
 const TeacherList: React.FC = () => {
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são os proffys disponíveis.">
         <form id="search-teachers">
-          <Input label="Matéria" name="subject" />
-          <Input label="Dia da semana" name="week_day" />
+          <Select
+            name="subject"
+            label="Matéria"
+            options={[
+              { value: "Artes", label: "Artes" },
+              { value: "Biologia", label: "Biologia" },
+              { value: "Ciências", label: "Ciências" },
+              { value: "Educação Física", label: "Educação Física" },
+              { value: "Física", label: "Física" },
+              { value: "Matemática", label: "Matemática" },
+              { value: "Química", label: "Química" },
+              { value: "Português", label: "Português" },
+              { value: "História", label: "História" },
+            ]}
+          />
+          <Select
+            name="week_day"
+            label="Dia da semana"
+            options={[
+              { value: "0", label: "Domingo" },
+              { value: "1", label: "Segunda-feira" },
+              { value: "2", label: "Terça-feira" },
+              { value: "3", label: "Quarta-feira" },
+              { value: "4", label: "Quinta-feira" },
+              { value: "5", label: "Sexta-feira" },
+              { value: "6", label: "Sábado" },
+            ]}
+          />
           <Input type="time" label="Hora" name="time" />
         </form>
       </PageHeader>
