d7bdbb9afa9d9cf74afcbad8b387dc1baeff9d9a
guiguat <guaturadzn@gmail.com>
2020-08-06 18:43:50 -0300

    create ui

--- web/src/Pages/TeacherForm/index.tsx
+++ web/src/Pages/TeacherForm/index.tsx
@@ -3,6 +3,9 @@ import PageHeader from "../../Components/PageHeader";
 
 import "./styles.css";
 import Input from "../../Components/Input";
+import warningIcon from "../../assets/images/icons/warning.svg";
+import Textarea from "../../Components/Textarea";
+import Select from "../../Components/Select";
 
 const TeacherForm: React.FC = () => {
   return (
@@ -17,7 +20,58 @@ const TeacherForm: React.FC = () => {
           <Input name="name" label="Nome Completo" />
           <Input name="avatar" label="Avatar" />
           <Input name="whatsapp" label="WhatsApp" />
+          <Textarea name="bio" label="Biografia" />
         </fieldset>
+        <fieldset>
+          <legend>Sobre a aula</legend>
+          <Select
+            name="subject"
+            label="Matéria"
+            options={[
+              { value: "Artes", label: "Artes" },
+              { value: "Biologia", label: "Biologia" },
+              { value: "Ciências", label: "Ciências" },
+              { value: "Educação Física", label: "Educação Física" },
+              { value: "Física", label: "Física" },
+              { value: "Matemática", label: "Matemática" },
+              { value: "Química", label: "Química" },
+              { value: "Português", label: "Português" },
+              { value: "História", label: "História" },
+            ]}
+          />
+          <Input name="cost" label="Custo da sua hora por aula" />
+        </fieldset>
+        <fieldset>
+          <legend>
+            Horários disponíveis
+            <button type="button"> + Novo Horário </button>
+          </legend>
+          <div className="schedule-item">
+            <Select
+              name="week_day"
+              label="Dia da semana"
+              options={[
+                { value: "0", label: "Domingo" },
+                { value: "1", label: "Segunda-feira" },
+                { value: "2", label: "Terça-feira" },
+                { value: "3", label: "Quarta-feira" },
+                { value: "4", label: "Quinta-feira" },
+                { value: "5", label: "Sexta-feira" },
+                { value: "6", label: "Sábado" },
+              ]}
+            />
+            <Input name="from" label="Das" type="time" />
+            <Input name="to" label="Até" type="time" />
+          </div>
+        </fieldset>
+        <footer>
+          <p>
+            <img src={warningIcon} alt="Aviso importante" />
+            Importante! <br />
+            Preencha todos os dados
+          </p>
+          <button type="button"> Salvar Cadastro </button>
+        </footer>
       </main>
     </div>
   );
