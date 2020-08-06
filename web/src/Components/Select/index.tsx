f13c72f9955fedc2bc798b2de799631fb488080d
guiguat <guaturadzn@gmail.com>
2020-08-06 18:43:22 -0300

    create select

---
+++ web/src/Components/Select/index.tsx
@@ -0,0 +1,29 @@
+import React, { SelectHTMLAttributes } from "react";
+
+import "./styles.css";
+
+interface ISelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
+  name: string;
+  label: string;
+  options: { value: string; label: string }[];
+}
+
+const Select: React.FC<ISelectProps> = ({ label, name, options, ...rest }) => {
+  return (
+    <div className="select-block">
+      <label htmlFor={name}>{label}</label>
+      <select id={name} {...rest}>
+        <option value="" disabled selected hidden>
+          Selecione um(a) {label.toLowerCase()}
+        </option>
+        {options.map((o) => (
+          <option key={o.value} value={o.value}>
+            {o.label}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default Select;
