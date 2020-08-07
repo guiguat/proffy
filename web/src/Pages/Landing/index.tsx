c43375a81d239b607711e1bf602a7b1b27b61240
guiguat <guaturadzn@gmail.com>
2020-08-06 21:38:39 -0300

    get total connections

--- web/src/Pages/Landing/index.tsx
+++ web/src/Pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import logoImg from "../../assets/images/logo.svg";
 import landingImg from "../../assets/images/landing.svg";
@@ -8,8 +8,19 @@ import purpleHeartIcon from "../../assets/images/icons/purple-heart.svg";
 
 import "./styles.css";
 import { Link } from "react-router-dom";
+import api from "../../services/api";
 
 const Landing: React.FC = () => {
+  const [totalConnections, setTotalConnections] = useState(0);
+
+  useEffect(() => {
+    async function getTotalConnections() {
+      const res = await api.get("/connections");
+      setTotalConnections(res.data.total);
+    }
+    getTotalConnections();
+  }, []);
+
   return (
     <div id="page-landing">
       <div id="page-landing-content" className="container">
@@ -33,7 +44,7 @@ const Landing: React.FC = () => {
           </Link>
         </div>
         <span className="total-connections">
-          Total de 200 conexões já realizadas{" "}
+          Total de {totalConnections} conexões já realizadas
           <img src={purpleHeartIcon} alt="Coração roxo" />
         </span>
       </div>
