ca9331160e8108fb5da06c49188ea3e447de6765
guiguat <guaturadzn@gmail.com>
2020-08-07 18:10:12 -0300

    create page header

--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -2,9 +2,14 @@ import React from "react";
 import { View } from "react-native";
 
 import styles from "./styles";
+import PageHeader from "../../components/PageHeader";
 
 const TeacherList: React.FC = () => {
-  return <View style={styles.container}></View>;
+  return (
+    <View style={styles.container}>
+      <PageHeader title="Proffys disponíveis" />
+    </View>
+  );
 };
 
 export default TeacherList;
