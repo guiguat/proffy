c94d3835a932b243c6cee5ecddd649c14fbca7c0
guiguat <guaturadzn@gmail.com>
2020-08-08 21:44:57 -0300

    create form

--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,15 +1,42 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Text } from "react-native";
 
 import styles from "./styles";
 import PageHeader from "../../components/PageHeader";
 import TeacherItem from "../../components/TeacherItem";
-import { ScrollView } from "react-native-gesture-handler";
+import { ScrollView, TextInput } from "react-native-gesture-handler";
 
 const TeacherList: React.FC = () => {
   return (
     <View style={styles.container}>
-      <PageHeader title="Proffys disponíveis" />
+      <PageHeader title="Proffys disponíveis">
+        <View style={styles.searchForm}>
+          <Text style={styles.label}>Matéria</Text>
+          <TextInput
+            style={styles.input}
+            placeholderTextColor="#c1bccc"
+            placeholder="Qual a matéria"
+          />
+          <View style={styles.inputGroup}>
+            <View style={styles.inputBlock}>
+              <Text style={styles.label}>Dia da semana</Text>
+              <TextInput
+                style={styles.input}
+                placeholderTextColor="#c1bccc"
+                placeholder="Qual o dia?"
+              />
+            </View>
+            <View style={styles.inputBlock}>
+              <Text style={styles.label}>Horário</Text>
+              <TextInput
+                style={styles.input}
+                placeholderTextColor="#c1bccc"
+                placeholder="Qual horário?"
+              />
+            </View>
+          </View>
+        </View>
+      </PageHeader>
       <ScrollView
         style={styles.teacherList}
         contentContainerStyle={{ paddingHorizontal: 16, paddingBottom: 16 }}
--- mobile/src/pages/TeacherList/styles.ts
+++ mobile/src/pages/TeacherList/styles.ts
@@ -8,6 +8,29 @@ const styles = StyleSheet.create({
   teacherList: {
     marginTop: -40,
   },
+  searchForm: {
+    marginBottom: 24,
+  },
+  label: {
+    color: "#d4c2ff",
+    fontFamily: "Poppins_400Regular",
+  },
+  input: {
+    height: 54,
+    backgroundColor: "#FFF",
+    borderRadius: 8,
+    justifyContent: "center",
+    paddingHorizontal: 16,
+    marginTop: 4,
+    marginBottom: 16,
+  },
+  inputGroup: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  inputBlock: {
+    width: "48%",
+  },
 });
 
 export default styles;
