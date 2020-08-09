f433724e15e5f7008547c8b7ab5eb313757f0974
guiguat <guaturadzn@gmail.com>
2020-08-08 21:25:05 -0300

    add items to scrollview

--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -3,11 +3,23 @@ import { View } from "react-native";
 
 import styles from "./styles";
 import PageHeader from "../../components/PageHeader";
+import TeacherItem from "../../components/TeacherItem";
+import { ScrollView } from "react-native-gesture-handler";
 
 const TeacherList: React.FC = () => {
   return (
     <View style={styles.container}>
       <PageHeader title="Proffys disponíveis" />
+      <ScrollView
+        style={styles.teacherList}
+        contentContainerStyle={{ paddingHorizontal: 16, paddingBottom: 16 }}
+      >
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+      </ScrollView>
     </View>
   );
 };
