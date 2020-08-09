173dce3b198b0c1f07aa8deccae40db0e21cda7f
guiguat <guaturadzn@gmail.com>
2020-08-08 22:09:10 -0300

    getTotalConnections

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { View, Text, Image, TouchableOpacity } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, Image, TouchableOpacity, Alert } from "react-native";
 
 import styles from "./styles";
 import landingImg from "../../assets/images/landing.png";
@@ -8,9 +8,19 @@ import giveClassesIcon from "../../assets/images/icons/give-classes.png";
 import heartIcon from "../../assets/images/icons/heart.png";
 import { useNavigation } from "@react-navigation/native";
 import { RectButton } from "react-native-gesture-handler";
+import api from "../../services/api";
 
 const Landing: React.FC = () => {
   const navigation = useNavigation();
+  const [totalConnections, setTotalConnections] = useState(0);
+  useEffect(() => {
+    api
+      .get("connections")
+      .then((res) => setTotalConnections(res.data.total))
+      .catch((err) =>
+        Alert.alert(`Erro ao se conectar com o servidor.\n Erro: ${err}`)
+      );
+  }, []);
 
   function handleNavigateToGiveClassesPage() {
     navigation.navigate("GiveClasses");
@@ -43,7 +53,8 @@ const Landing: React.FC = () => {
         </RectButton>
       </View>
       <Text style={styles.totalConnections}>
-        Total de 285 conexões já realizadas <Image source={heartIcon} />
+        Total de {totalConnections} conexões já realizadas{" "}
+        <Image source={heartIcon} />
       </Text>
     </View>
   );
