47ae8ffcd9aaffc68fca5036a3f5c98e51b71726
guiguat <guaturadzn@gmail.com>
2020-08-11 20:22:00 -0300

    create table users

---
+++ backend/src/database/migrations/04_create_user.ts
@@ -0,0 +1,13 @@
+import Knex from "knex";
+
+export async function up(knex: Knex) {
+  return knex.schema.createTable("user", (table) => {
+    table.increments("id").primary();
+    table.string("name").notNullable();
+    table.string("email").notNullable();
+    table.string("password").notNullable();
+  });
+}
+export async function down(knex: Knex) {
+  return knex.schema.dropTable("user");
+}
