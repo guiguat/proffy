57479fe3175d2ab7404ee9dead687865c082272b
guiguat <guaturadzn@gmail.com>
2020-08-11 20:40:40 -0300

    use static methods

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,16 +1,13 @@
-import express from 'express';
-import ClassesController from './controllers/ClassesController';
-import ConnectionsController from './controllers/ConnectionsController';
+import express from "express";
+import ClassesController from "./controllers/ClassesController";
+import ConnectionsController from "./controllers/ConnectionsController";
 
 const routes = express.Router();
 
-const classesController = new ClassesController();
-const connectionsController = new ConnectionsController();
+routes.post("/classes", ClassesController.create);
+routes.get("/classes", ClassesController.index);
 
-routes.post('/classes', classesController.create);
-routes.get('/classes', classesController.index);
-
-routes.post('/connections', connectionsController.create);
-routes.get('/connections', connectionsController.index);
+routes.post("/connections", ConnectionsController.create);
+routes.get("/connections", ConnectionsController.index);
 
 export default routes;
