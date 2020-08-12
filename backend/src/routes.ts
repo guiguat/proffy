de08c7ae2d5530359c3ca9dfef446ce97c366077
guiguat <guaturadzn@gmail.com>
2020-08-11 23:31:48 -0300

    test middleware

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -2,6 +2,8 @@ import express from "express";
 import ClassesController from "./controllers/ClassesController";
 import ConnectionsController from "./controllers/ConnectionsController";
 import UsersController from "./controllers/UsersController";
+import db from "./database/connection";
+import authMiddleware from "./middlewares/auth";
 
 const routes = express.Router();
 
@@ -13,5 +15,17 @@ routes.get("/connections", ConnectionsController.index);
 
 routes.post("/users", UsersController.create);
 routes.get("/auth", UsersController.auth);
+routes.use(authMiddleware);
+routes.get("/me", async (req, res) => {
+  try {
+    const { userId }: any = req;
+
+    const user = await db("user").select("*").where("id", userId);
+
+    return res.json({ user });
+  } catch (err) {
+    return res.status(400).json({ error: "Can't get user information" });
+  }
+});
 
 export default routes;
