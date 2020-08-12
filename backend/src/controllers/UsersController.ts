5e0463fcf45ba09533e3047815f39a6100edb82f
guiguat <guaturadzn@gmail.com>
2020-08-11 21:11:16 -0300

    create usersController

---
+++ backend/src/controllers/UsersController.ts
@@ -0,0 +1,26 @@
+import { Request, Response, NextFunction } from "express";
+
+import db from "../database/connection";
+import bcrypt from "bcrypt";
+
+interface IUser {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export default class UsersController {
+  static async create(req: Request, res: Response, next: NextFunction) {
+    const { name, email, password }: IUser = req.body;
+
+    try {
+      const securePassword = bcrypt.hashSync(password, bcrypt.genSaltSync(13));
+      await db("user").insert({ name, email, password: securePassword });
+      return res.status(201).json({ message: "Usuario criado com sucesso!" });
+    } catch (err) {
+      next(err);
+    }
+
+    res.status(201).json({ message: "Usuario criado com sucesso!" });
+  }
+}
