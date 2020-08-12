150c18c8201f4ff9f736a7d0dc74197c2193f5ab
guiguat <guaturadzn@gmail.com>
2020-08-12 14:37:38 -0300

    change expiration time

--- backend/src/utils/jwt.ts
+++ backend/src/utils/jwt.ts
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 export function genToken(id: string) {
   return jwt.sign({ id }, String(process.env.ACCESS_TOKEN_SECRET), {
-    expiresIn: 86400,
+    expiresIn: "15m",
   });
 }
