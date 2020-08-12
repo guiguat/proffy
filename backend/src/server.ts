c80bcdb5509eba8d1b6e1dc3f095dbc85aef76ac
guiguat <guaturadzn@gmail.com>
2020-08-11 21:07:44 -0300

    create error handler

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,23 @@
-import express from 'express';
-import cors from 'cors';
-import routes from './routes';
+import express, { Request, Response, NextFunction } from "express";
+import cors from "cors";
+import routes from "./routes";
+
+interface ResponseError extends Error {
+  status?: number;
+}
 
 const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 app.use(routes);
-
-app.listen(3333)
+app.use(
+  (error: ResponseError, req: Request, res: Response, next: NextFunction) => {
+    res.status(error.status || 500);
+    res.json({
+      message: "Ocorreu um erro ao completar ação, tente novamente.",
+      error: error.message,
+    });
+  }
+);
+app.listen(3333);
