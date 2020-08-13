9315269b38ae95c11e856999f62ebd6996a1694f
guiguat <guaturadzn@gmail.com>
2020-08-12 23:15:11 -0300

    create authentication methods

--- backend/src/controllers/UsersController.ts
+++ backend/src/controllers/UsersController.ts
@@ -3,6 +3,10 @@ import { Request, Response, NextFunction } from "express";
 import db from "../database/connection";
 import bcrypt from "bcrypt";
 import { genToken } from "../utils/jwt";
+import IMailProvider from "../providers/IMailProvider";
+import crypto from "crypto";
+import MailtrapMailProvider from "../providers/MailtrapMailProvider";
+
 interface IUser {
   name: string;
   email: string;
@@ -10,7 +14,7 @@ interface IUser {
 }
 
 export default class UsersController {
-  static async create(req: Request, res: Response, next: NextFunction) {
+  async create(req: Request, res: Response, next: NextFunction) {
     const { name, email, password }: IUser = req.body;
     try {
       const securePassword = bcrypt.hashSync(password, bcrypt.genSaltSync(13));
@@ -21,7 +25,7 @@ export default class UsersController {
     }
   }
 
-  static async auth(req: Request, res: Response, next: NextFunction) {
+  async auth(req: Request, res: Response, next: NextFunction) {
     const { email, password } = req.body;
     try {
       const [user] = await db("user").select("*").where("email", email);
@@ -38,6 +42,76 @@ export default class UsersController {
       }
     } catch (err) {
       next(err);
+    }
+  }
+
+  async forgotPassword(req: Request, res: Response, next: NextFunction) {
+    const { email } = req.query;
+    console.log();
+    try {
+      const [user] = await db("user")
+        .select("*")
+        .where("email", "=", String(email));
+      if (!user) {
+        return res.status(400).json({ error: "Usuário não encontrado" });
+      }
+      const pwd_reset_token = crypto.randomBytes(20).toString("hex");
+      const pwd_reset_expires = new Date();
+      pwd_reset_expires.setHours(pwd_reset_expires.getHours() + 1);
+
+      await db("user")
+        .update({ pwd_reset_token, pwd_reset_expires })
+        .where("email", "=", String(email));
+
+      const mailProvider = new MailtrapMailProvider();
+      await mailProvider.sendMail(
+        {
+          from: { email: "guaturadzn@gmail.com", name: "Guilherme Guatura" },
+          to: { email: user.email, name: user.name },
+          template: "auth/forgot_password",
+          subject: "Recuperar senha",
+          context: { token: pwd_reset_token },
+        },
+        (err) => {
+          if (err) {
+            console.log(err);
+            return res
+              .status(400)
+              .json({ error: "Não foi possível enviar email de recuperação" });
+          }
+          return res.status(201).send();
+        }
+      );
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async resetPassword(req: Request, res: Response, next: NextFunction) {
+    const { email, token, password } = req.body;
+    try {
+      const [user] = await db
+        .from("user")
+        .where("email", "=", email)
+        .select("pwd_reset_token", "pwd_reset_expires");
+      if (!user)
+        return res.status(400).json({ error: "Usuario não encontrado" });
+
+      if (token != user.pwd_reset_token)
+        return res.json({ error: "Token inválido" });
+
+      const now = new Date();
+
+      if (now > user.pwd_reset_expires)
+        return res.status(400).json({ error: "Token expirado, gere um novo!" });
+
+      const securePassword = bcrypt.hashSync(password, bcrypt.genSaltSync(13));
+      await db("user")
+        .update({ password: securePassword })
+        .where("email", "=", email);
+      return res.status(201).json({ message: "Senha alterada com sucesso!" });
+    } catch (error) {
+      next(error);
     }
   }
 }
