7343b42783d364aec11280b644ec34976c309500
guiguat <guaturadzn@gmail.com>
2020-08-15 12:15:10 -0300

    change name

--- backend/src/utils/convertHourToMinutes.ts
+++
@@ -1,5 +0,0 @@
-export default function convertHourToMinutes(time:string){
-    const [hours, minutes] = time.split(':');
-    const timeInMinutes = (parseInt(hours) * 60) + parseInt(minutes);
-    return timeInMinutes;
-}
