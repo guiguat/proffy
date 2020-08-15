f69c0570da8d244cc33711ce01aaf4542273e871
guiguat <guaturadzn@gmail.com>
2020-08-15 18:10:32 -0300

    use authmiddleware

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -23,7 +23,7 @@ routes.post("/connections", ConnectionsController.create);
 routes.get("/connections", ConnectionsController.index);
 
 routes.post("/user", UserController.create);
-routes.get("/user/:uid", UserController.index);
+routes.get("/user/:uid", authMiddleware, UserController.index);
 
 routes.get("/auth", authController.login);
 routes.get("/auth/reset_password", authController.forgotPassword);
