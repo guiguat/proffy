9f07116fa3365aafc416f570580507769ee3096b
guiguat <guaturadzn@gmail.com>
2020-08-15 18:09:22 -0300

    return all user data on index

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from "express";
 
 import db from "../database/connection";
 import bcrypt from "bcrypt";
+import { convertMinutesToHour } from "../utils/timeConversion";
 
 interface IUser {
   id?: number;
@@ -23,12 +24,55 @@ export default class UserController {
   }
 
   static async index(req: Request, res: Response, next: NextFunction) {
-    const { id } = req.params;
+    const { uid } = req.params;
+    if (!uid) {
+      return res.status(400).json({
+        error: "Id de usuário não especificado",
+      });
+    }
+
     try {
-      const [user]: IUser[] = await db("user")
-        .select("id", "name", "email")
-        .where("id", "=", id);
-      return res.json(user);
+      const [prof] = await db("professor").select("id").where("user_id", uid);
+      if (!prof) {
+        const [user] = await db("user")
+          .select("name", "email")
+          .where("id", uid);
+        return res.json(user);
+      }
+      const classes = await db("classes")
+        .where("prof_id", prof.id)
+        .join("professor", "classes.prof_id", "=", "professor.id")
+        .select(["classes.*", "professor.*"]);
+
+      const classesWithSchedule = await Promise.all(
+        classes.map(async ({ id, ...rest }) => {
+          const schedule = await db("classes")
+            .join(
+              "class_schedule",
+              "classes.id",
+              "=",
+              "class_schedule.class_id"
+            )
+            .select(["class_schedule.*"])
+            .where("class_schedule.class_id", id);
+          const scheduleTimeConverted = schedule.map((scheduleItem) => {
+            return {
+              weekday: scheduleItem.weekday,
+              from: convertMinutesToHour(scheduleItem.from),
+              to: convertMinutesToHour(scheduleItem.to),
+            };
+          });
+
+          const [{ email }] = await db("user").select("email").where("id", uid);
+
+          return {
+            ...rest,
+            email,
+            schedule: scheduleTimeConverted,
+          };
+        })
+      );
+      return res.json(classesWithSchedule);
     } catch (error) {
       next(error);
     }
