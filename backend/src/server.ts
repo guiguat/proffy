3299775fb992b7dc9ca5c9a727fabdc758efbe7a
guiguat <guaturadzn@gmail.com>
2020-08-14 23:47:17 -0300

    clean code

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,23 +1,11 @@
 import express, { Request, Response, NextFunction } from "express";
 import cors from "cors";
 import routes from "./routes";
+import errorMiddleware from "./middlewares/errorhandler";
 require("dotenv").config();
-interface ResponseError extends Error {
-  status?: number;
-}
-
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.use(
-  (error: ResponseError, req: Request, res: Response, next: NextFunction) => {
-    res.status(error.status || 500);
-    res.json({
-      message: "Ocorreu um erro ao completar ação, tente novamente.",
-      error: error.message,
-    });
-  }
-);
+app.use(errorMiddleware);
 app.listen(3333);
