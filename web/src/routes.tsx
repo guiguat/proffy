83c8751245aa621d1be10976e37deec8698e4d3f
guiguat <guaturadzn@gmail.com>
2020-08-23 22:28:25 -0300

    update routes

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -3,12 +3,14 @@ import { BrowserRouter, Route } from "react-router-dom";
 import Landing from "./Pages/Landing";
 import TeacherList from "./Pages/TeacherList";
 import TeacherForm from "./Pages/TeacherForm";
+import Login from "./Pages/Login";
 // import { Container } from './styles';
 
 const Routes: React.FC = () => {
   return (
     <BrowserRouter>
-      <Route path="/" exact component={Landing} />
+      <Route path="/" exact component={Login} />
+      <Route path="/home" exact component={Landing} />
       <Route path="/study" component={TeacherList} />
       <Route path="/give-classes" component={TeacherForm} />
     </BrowserRouter>
