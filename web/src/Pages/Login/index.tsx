54d69a4a32cbeda81ecb9f6e18e81708c840617e
guiguat <guaturadzn@gmail.com>
2020-08-23 22:00:53 -0300

    create login page

---
+++ web/src/Pages/Login/index.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+import "./styles.css";
+import Input from "../../Components/Input";
+import logoImg from "../../assets/images/logo.svg";
+import purpleHeartIcon from "../../assets/images/icons/purple-heart.svg";
+
+const Login: React.FC = () => {
+  return (
+    <div className="login-page">
+      <header>
+        <img src={logoImg} alt="Profy" />
+        <h2>Sua plataforma de estudos online</h2>
+      </header>
+      <main>
+        <form id="login-form">
+          <fieldset>
+            <legend>Fazer Login</legend>
+            <Input name="email" label="Email" />
+            <Input name="password" label="Senha" />
+            <div className="login-extra">
+              <div>
+                <input name="remember-me" id="remember-me" type="checkbox" />
+                <label htmlFor="remember-me">Lembrar-me</label>
+              </div>
+              <a href="/">Esqueci minha senha</a>
+            </div>
+            <button>Entrar</button>
+          </fieldset>
+        </form>
+        <footer>
+          <p>
+            Não tem conta? <br /> <a href="/">Cadastre-se</a>
+          </p>
+          <span>
+            É de graça <img src={purpleHeartIcon} alt="coração" />
+          </span>
+        </footer>
+      </main>
+    </div>
+  );
+};
+
+export default Login;
